8ae048a6bb111dbbb0e48f96d733039556a7f2bc
hanieh.sharifi <hanieh.sharifi@divar.ir>
2023-08-26 17:13:23 +0330

    feat: add rate and english digit transformer

---
+++ src/views/Vendors/components/Star/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+function Star() {
+  return (
+    <>
+      <FontAwesomeIcon icon={faStar} />
+    </>
+  );
+}
+
+export default Star;
