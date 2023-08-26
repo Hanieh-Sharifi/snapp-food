354941f4c1220d33e489fa2fb5eb0a5f1ec08cea
hanieh.sharifi <hanieh.sharifi@divar.ir>
2023-08-26 18:59:30 +0330

    feat: add pagination for cards

--- src/views/Vendors/index.jsx
+++ src/views/Vendors/index.jsx
@@ -10,38 +10,83 @@ import "./Vendors.scss";
 
 function Vendors() {
   const dispatch = useDispatch();
-  const contents = useSelector((state) => state.contents);
   const isLoading = useSelector((state) => state.isLoading);
   const error = useSelector((state) => state.error);
+  const vendors = useSelector((state) => state.vendors);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    dispatch(getVendors());
+    dispatch(getVendors(currentPage));
   }, []);
+
+  useEffect(() => {
+    if (vendors.length > 0) {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0;
+    }
+  }, [vendors]);
+
+  function loadNextPage() {
+    dispatch(getVendors(currentPage + 1));
+    setCurrentPage((currentPage) => currentPage + 1);
+  }
+  function loadPrevPage() {
+    dispatch(getVendors(currentPage - 1));
+    setCurrentPage((currentPage) => currentPage - 1);
+  }
+
+  function shouldRenderButtons() {
+    if (!isLoading && vendors.length > 0) {
+      return true;
+    }
+
+    return false;
+  }
 
   return (
     <div className="vendors">
       {isLoading && <Progress />}
-      {contents?.data.finalResult?.map((widget) => {
-        if (widget.type === "TEXT") {
-          return;
-        }
+      {!isLoading &&
+        vendors?.map((widget) => {
+          if (widget.type === "TEXT") {
+            return;
+          }
 
-        const { backgroundImage, deliveryFee, description, rate, logo, title } =
-          widget.data;
+          const {
+            backgroundImage,
+            deliveryFee,
+            description,
+            rate,
+            logo,
+            title,
+          } = widget.data;
 
-        return (
-          <Card
-            backgroundImage={backgroundImage}
-            deliveryFee={deliveryFee}
-            description={description}
-            rate={rate}
-            logo={logo}
-            title={title}
-            // deliveryType={deliveryType}
-            key={title}
-          />
-        );
-      })}
+          return (
+            <Card
+              backgroundImage={backgroundImage}
+              deliveryFee={deliveryFee}
+              description={description}
+              rate={rate}
+              logo={logo}
+              title={title}
+              key={title}
+            />
+          );
+        })}
+      {shouldRenderButtons() && (
+        <div className="vendors__pagination">
+          <button onClick={loadNextPage} className="vendors__pagination-button">
+            nextPage
+          </button>
+          <button
+            onClick={loadPrevPage}
+            disabled={currentPage === 0}
+            className="vendors__pagination-button"
+          >
+            prevPage
+          </button>{" "}
+        </div>
+      )}
       {error && <div>fetch error</div>}
     </div>
   );
