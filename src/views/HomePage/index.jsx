395cbdbfe311d996c6344b9c373c0dbd044aecc2
hanieh.sharifi <hanieh.sharifi@divar.ir>
2023-08-26 09:28:27 +0330

    refactor: ignore dist in gitignrore file

---
+++ src/views/HomePage/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { VENDORS_PATH } from "../../configs/paths";
+
+import "./HomePage.scss";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  function handleButtonClick() {
+    navigate(VENDORS_PATH);
+  }
+
+  return (
+    <button type="button" class="home-page__button" onClick={handleButtonClick}>
+      HomePage
+    </button>
+  );
+}
+
+export default HomePage;
