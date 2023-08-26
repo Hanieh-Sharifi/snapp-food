0f67c4b11ed8576d9de56dae0fa2ea931246ad27
Hanieh-Sharifi <80568177+Hanieh-Sharifi@users.noreply.github.com>
2023-08-26 10:01:50 +0330

    feat: add loading for verndors request (#5)
    Co-authored-by: hanieh.sharifi <hanieh.sharifi@divar.ir>

--- src/views/Vendors/index.jsx
+++ src/views/Vendors/index.jsx
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 
+import Progress from "./components/Progress";
+
 import { getVendorsList } from "./request";
 
 function Vendors() {
   const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    getVendorsList();
+    getVendorsList()
+      .then((data) => {
+        setData(data);
+      })
+      .catch(() => setHasError(true))
+      .finally(setIsLoading(false));
   }, []);
-  return <div>Vendors</div>;
+
+  return (
+    <div>
+      {isLoading && <Progress />}
+      <div>random</div>
+    </div>
+  );
 }
 
 export default Vendors;
--- src/views/Vendors/request.js
+++ src/views/Vendors/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function getVendorsList() {
-  axios
+  return axios
     .get(
       "https://snappfood.ir/mobile/v3/restaurant/vendors-list?page=0&page_size=10&lat=35.754&long=51.328"
     )
